dbb3b0609d1ebd28a4c1fcd6c9198a3fb2d53afa
solbencas <cecibenitezca@gmail.com>
2020-11-26 08:34:33 -0300

    Add some styles

--- js/main.js
+++ js/main.js
@@ -13,6 +13,7 @@ $form = document.querySelector('.form');
 $titleInput = $form.querySelector('#title');
 $authorInput = $form.querySelector('#author');
 $submitButton = $form.querySelector('#submit');
+$returnButon = $form.querySelector('#return');
 
 function Book(title, author) {
   // the constructor...
@@ -24,6 +25,7 @@ const addBookToLibrary = () => {
   // do stuff here
   let title = $titleInput.value;
   let author = $authorInput.value;
+
   let newBook = new Book(title, author);
   myLibrary.push(newBook);
   console.log('something');
@@ -37,13 +39,44 @@ const getStorage = () => {
   myLibrary = JSON.parse(localStorage.getItem('library'));
 }
 
+const toggleHiddenElements = () => {
+  $form.classList.toggle('hidden');
+  $table.classList.toggle('hidden');
+  $newButton.classList.toggle('hidden');
+}
+
+const validateForm = () => {
+  if ($titleInput.value == "" && document.querySelector('#titleError') == null);
+  if ($authorInput.value == "" && document.querySelector('#authorError') == null);
+
+  if ($titleInput.value == "" || $authorInput.value == "") return false;
+  else return true;
+}
+
+const clearForm = () => {
+  $titleInput.value = null;
+  $authorInput.value = null;
+}
+
 document.addEventListener('DOMContentLoaded', () => {
-  //$newButton.addEventListener('click', toggleHiddenElements);
+  $newButton.addEventListener('click', toggleHiddenElements);
 
   $submitButton.addEventListener('click', () => {
     // if validation
     addBookToLibrary();
+    toggleHiddenElements();
     // some more stuff
   })
+
+  $returnButon.addEventListener('click', () => {
+    toggleHiddenElements();
+    //
+  });
+
+  if(!localStorage.getItem('library')) {
+    populateStorage();
+  } else {
+    getStorage();
+  }
 })
 
